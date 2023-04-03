5f640c35ab24031679851835447d351c5d65001b
msKaleb <sjmikel@gmx.es>
2023-04-03 11:58:47 +0200

    lun 03 abr 2023 11:58:47 CEST

--- main_bonus.c
+++ main_bonus.c
@@ -6,21 +6,60 @@
 /*   By: msoria-j < msoria-j@student.42urduliz.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 14:55:08 by msoria-j          #+#    #+#             */
-/*   Updated: 2023/04/02 15:17:11 by msoria-j         ###   ########.fr       */
+/*   Updated: 2023/04/03 11:58:23 by msoria-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex_bonus.h"
+#include "./pipex_bonus.h"
+
+void	here_doc(char *outfile, char *limiter)
+{
+	int		fd;
+	int		i, j;
+	char	*buffer;
+	
+	buffer = ft_calloc(1, 1000 * sizeof(buffer));
+	ft_fprintf(2, "Limiter: %s\n", limiter);
+	fd = open(outfile, O_RDWR | O_CREAT | O_APPEND);
+	i = -1;
+	j = 0;
+	while (++i < 1000)
+	{
+		read(STDIN_FILENO, buffer, 1);
+		if (*buffer == '\n')
+		{
+			buffer++;
+			i++;
+			j += i;
+			if (ft_strnstr(buffer - i, limiter, ft_strlen(limiter)))
+				break ;
+			else
+			{
+				// ft_fprintf(fd, "%s", buffer - j); // cambiar por execve
+				i = -1;
+				/* free(buffer);
+				buffer = ft_calloc(1, 1000 * sizeof(buffer)); */
+			}
+		}
+		else
+			buffer++;
+	}
+	// ft_fprintf(2, "j - i: %d\n%s\n", j - i, buffer - j);
+	buffer -= j;
+	buffer[j - i] = '\0';
+	ft_fprintf(fd, "%s\n", buffer);
+	
+	free(buffer);
+	close(fd);
+	exit(0);
+}
 
 /* Check the argument number */
-static void	check_argc(int argc)
+static void	check_argc(int argc, char **argv)
 {
-	if (argc > 5)
-	{
-		errno = E2BIG;
-		perror("Error");
-		exit(errno);
-	}
+	if (ft_strnstr(argv[1], "here_doc", 8))
+		// ft_fprintf(2, "here_doc\n");
+		here_doc(argv[argc - 1], argv[2]);
 	if (argc < 5)
 	{
 		errno = EINVAL;
@@ -35,7 +74,7 @@ int	main(int argc, char **argv, char **envp)
 	t_paths			p;
 	int				i;
 
-	check_argc(argc);
+	check_argc(argc, argv);
 	p.argv = argv;
 	p.envp = envp;
 	i = 0;
